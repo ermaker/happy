b50134c10f09daa8a6b29d63abee5a8369ea7e1a
Minwoo Lee <ermaker@gmail.com>
2015-03-03 11:32:07 +0900

    Refactory

--- lib/happy/collector.rb
+++ lib/happy/collector.rb
@@ -2,11 +2,15 @@ require 'happy/xrp'
 
 module Happy
   class Collector
+    def initialize
+      @logstash = Happy.logstash.with(type: 'market_prices')
+    end
+
     def log_market_xrp_impl(base, counter)
       xrp = XRP.new(ENV['XRP_ADDRESS'])
       asks = xrp.market(base, counter)
       Happy.logger.debug { "Count of asks(#{base}, #{counter}): #{asks.size}" }
-      Happy.logstash.with(type: 'test').at_once.stash_all(asks)
+      @logstash.at_once.stash_all(asks)
     end
 
     def log_market_xrp
@@ -18,7 +22,7 @@ module Happy
       xcoin = XCoin.new
       asks = xcoin.market
       Happy.logger.debug { "Count of asks(XCoin): #{asks.size}" }
-      Happy.logstash.with(type: 'test').at_once.stash_all(asks)
+      @logstash.at_once.stash_all(asks)
     end
   end
 end
